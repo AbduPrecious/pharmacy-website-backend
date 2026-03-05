4527077532d0399e5d94013c2219951c219d4a58
AbduPrecious <abdurezakberiso@gmail.com>
2026-03-05 14:43:20 +0300

    fix: use DATABASE_URL for PostgreSQL connection

--- config/database.js
+++ config/database.js
@@ -1,16 +1,12 @@
-const path = require('path');
-
+// config/database.js
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'pharmacy_db'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi123'),
-      schema: env('DATABASE_SCHEMA', 'public'),
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', true) && {
+        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+      },
     },
-    debug: false,
   },
 });
